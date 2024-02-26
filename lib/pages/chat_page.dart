65d4c2ac9603bac363e2eb69d38a6fdec18f9d79
Jota <jalvarez@lsv-tech.com>
2024-02-25 22:10:54 -0500

    chat page finish

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,14 +1,161 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/index.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
   @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
+  final textController = TextEditingController();
+  final _focusNode = FocusNode();
+  bool _isWriting = false;
+
+  final List<ChatMessage> _messages = [
+    // const ChatMessage(text: "Hola Mundo", uid: "123"),
+    // const ChatMessage(text: "Hola Mundo", uid: "1234"),
+    // const ChatMessage(text: "Hola Mundo", uid: "123"),
+    // const ChatMessage(text: "Hola Mundo", uid: "1234"),
+    // const ChatMessage(text: "Hola Mundo", uid: "123"),
+    // const ChatMessage(text: "Hola Mundo", uid: "1234")
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 1,
+          centerTitle: true,
+          title: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.blue[100],
+                maxRadius: 13,
+                child: const Text(
+                  "Da",
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text(
+                "Juan Perez",
+                style: TextStyle(color: Colors.black87, fontSize: 14),
+              )
+            ],
+          ),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (_, index) => _messages[index],
+                  reverse: true,
+                ),
+              ),
+              const Divider(height: 1),
+              //Caja de texto
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: _inputChat(),
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String text) {
+                  setState(() {
+                    // ignore: prefer_is_empty
+                    if (text.trim().length > 0) {
+                      _isWriting = true;
+                    } else {
+                      _isWriting = false;
+                    }
+                  });
+                },
+                decoration:
+                    const InputDecoration.collapsed(hintText: "Send message"),
+                focusNode: _focusNode,
+              ),
+            ),
+            //buttom
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              child: Platform.isIOS
+                  ? CupertinoButton(
+                      onPressed: _isWriting
+                          ? () => _handleSubmit(textController.text.trim())
+                          : null,
+                      child: const Text("Send"),
+                    )
+                  : Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 4),
+                      child: IconTheme(
+                        data: const IconThemeData(color: Colors.blue),
+                        child: IconButton(
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          onPressed: _isWriting
+                              ? () => _handleSubmit(textController.text.trim())
+                              : null,
+                          icon: const Icon(
+                            Icons.send,
+                          ),
+                        ),
+                      ),
+                    ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  _handleSubmit(String text) {
+    if (text.isEmpty) return;
+    _focusNode.requestFocus();
+    textController.clear();
+
+    final newMessage = ChatMessage(
+      text: text,
+      uid: "123",
+      animationController: AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 200)),
+    );
+    _messages.insert(0, newMessage);
+    newMessage.animationController.forward();
+
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: off socket
+
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 }
--- lib/widgets/index.dart
+++ lib/widgets/index.dart
@@ -1,3 +1,5 @@
+export 'package:chat_app/widgets/chat_message.dart';
+
 export 'package:chat_app/widgets/custom_buttom.dart';
 
 export 'package:chat_app/widgets/labels.dart';
